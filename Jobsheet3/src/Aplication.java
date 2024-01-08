67aa6c8f589f29042dbf4684131fb839f9e8c218
indahiasha <117700822+indahiasha@users.noreply.github.com>
2024-01-08 23:04:11 +0700

    Add files via upload

---
+++ Jobsheet3/src/Aplication.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.jobsheet3;
+
+/**
+ *
+ * @author Indah Iasha - 22343022
+ */
+public class Application {
+    public static void main(String args[]) {
+        int myNumber = 88;
+        short myShort = 847;
+        long myLong = 9797L;
+        double myDouble = 72.97d;
+        float myFloat = 324.3f;
+        char myChar = 'y';
+        boolean myBoolean = false;
+        byte myByte = 127;
+        
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+        
+    }
+}
---
+++ Jobsheet3/src/Tugas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.jobsheet3;
+
+/**
+ *
+ * @author Indah Iasha - 22343022
+ */
+public class tugas {
+    public static void main(String[] args){
+        int number = 10;
+        char letter ='a';
+        boolean result = true;
+        String str = "hello";
+        
+        System.out.println("Number = " +number);
+        System.out.println("Letter = " +letter);
+        System.out.println("result = " +result);
+        System.out.println("str = " +str);
+        
+    }
+    
+}
