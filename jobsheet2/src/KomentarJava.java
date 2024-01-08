87330d86b30e917556adb8c048ff1b6bb710c740
indahiasha <117700822+indahiasha@users.noreply.github.com>
2024-01-08 23:01:17 +0700

    Add files via upload

---
+++ jobsheet2/src/KomentarJava.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.jobsheet2;
+
+/**
+ *
+ * @author Indah Iasha - 22343022
+ */
+public class KomentarJava {
+    public static void main(String args[]){
+        System.out.println("Single line comment above");
+        // Comment line 1
+        System.out.println("Multi line comment below");
+        /*  Comment line 2
+            Comment line 3
+            Comment line 4
+        */
+    }
+}
---
+++ jobsheet2/src/Tugas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.jobsheet2;
+
+/**
+ *
+ * @author Indah Iasha - 22343022
+ */
+public class Tugas {
+    public static void main(String args[]){
+        System.out.println("Ini adalah komentar satu baris");
+        // Komentar
+        System.out.println("Ini adalah komentar multi baris");
+        /*
+        Ini adalah Komentar Multi Baris
+        */
+        String kata = "Tidur";
+        String Kata = "tidur";
+        String KATA = "Turu";
+        System.out.println(kata);
+        System.out.println(Kata);
+        System.out.println(KATA);
+    }
+}
